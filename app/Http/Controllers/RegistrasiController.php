39f926a61231ca60064f27fc8b6868cdccf802fa
Gorthdzx <krislaksana123@gmail.com>
2024-05-22 05:51:17 +0700

    feat : hash password

--- app/Http/Controllers/RegistrasiController.php
+++ app/Http/Controllers/RegistrasiController.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
+
 class RegistrasiController extends Controller
 {
+    public function registrasi(): View
+    {
+        return view('registrasi.regis', ['title' => 'Registrasi']);
+    }
 
+    public function store(Request $request): RedirectResponse
+    {
+        $data = $request->all();
+        $user = new User;
+        $user->name = $data['name'];
+        $user->password = Hash::make($data['password']);
+        $user->save();
+        return redirect('/login');
+    }
 }
