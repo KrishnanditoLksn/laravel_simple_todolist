d32fd8c2be6fe0a3bae9ccdeed6a844fec92e9d4
Gorthdzx <krislaksana123@gmail.com>
2024-05-25 14:55:18 +0700

    feat : guest and auth middleware groups

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,34 @@ use App\Http\Controllers\RegistrasiController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-//get all todo's
-Route::get('/', [TodoController::class, 'index']);
 
-//add todo route
-Route::post('add-data', [TodoController::class, 'store']);
+Route::middleware(['auth'])->group(function () {
+    //get all todo's
+    Route::get('/todos', [TodoController::class, 'index']);
+
+    //add todo route
+    Route::post('add-data', [TodoController::class, 'store']);
+
+    //delete todo route
+    Route::get('/delete/{id}', [TodoController::class, 'delete']);
+    //update todo route
+    Route::get('/update/{id}', [TodoController::class, 'update']);
+    //logout user
+    Route::get('/logout', [LogoutController::class, 'logout']);
+
+});
 
 
-//delete todo route
-Route::get('/delete/{id}', [TodoController::class, 'delete']);
-//update todo route
-Route::get('/update/{id}', [TodoController::class, 'update']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'index']);
+    Route::get('/', [LoginController::class, 'index']);
+});
 
-
-//logout todo route
-Route::get('/login', [LoginController::class, 'index']);
-
-//ke halaman registrasi todo route
+//registrasi todo route
 Route::get('/registrasi', [RegistrasiController::class, 'registrasi']);
 
 //get id user untuk regis
 Route::post('registrasi-user', [RegistrasiController::class, 'store']);
 
 //get id user untuk login
-Route::post('login-user', [LoginController::class, 'auth']);
-
-//logout user
-Route::get('/logout', [LogoutController::class, 'logout']);
+Route::post('login-user', [LoginController::class, 'login']);
