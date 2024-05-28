ce84d016ccdb66a0d1517a19991c66b8b99a2256
Gorthdzx <krislaksana123@gmail.com>
2024-05-28 10:06:02 +0700

    feat : logout auth

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use http\Env\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class LogoutController extends Controller
 {
-    public function logout(\Illuminate\Http\Request $request):RedirectResponse{
+    public function logout(\Illuminate\Http\Request $request):View{
+        Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('index');
+        return view('login.index' , ['title'=>'Login']);
     }
 }
